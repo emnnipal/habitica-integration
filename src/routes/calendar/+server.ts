5c995ce776cfb0c7365f6af60215228639a4ba23
Emman Nipal <emnnipal@gmail.com>
2023-01-28 23:31:28 +0800

    chore: initial commit

---
+++ src/routes/calendar/+server.ts
@@ -0,0 +1,10 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import dayjs from 'dayjs';
+
+export const POST = (async ({ request }) => {
+  const body = await request.json();
+  console.log(`[${dayjs().format()}]`, 'body', JSON.stringify(body, null, 2));
+  return json({
+    message: 'Success',
+  });
+}) satisfies RequestHandler;
